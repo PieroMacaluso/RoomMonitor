ee7d0a001385293be7bbd468c529950134fe21f2
Piero Macaluso <piero.macaluso@studenti.polito.it>
2018-05-01 21:02:41 +0200

    Work advance ESP firmware
    L'algoritmo ora stampa i PROBE REQUEST correttamente, accompagnato da RSSI, MAC e SSID (opzionale). Provvederò presto a fare un refactoring e inserire i commenti nel codice per favorire la leggibilità.

--- ESP32firmware/main.c
+++ ESP32firmware/main.c
@@ -7,132 +7,138 @@
 #include "driver/gpio.h"
 #include <string.h>
 
+#define TYPE_MANAGEMENT       0x00
+#define TYPE_CONTROL          0x01
+#define TYPE_DATA             0x02
+
+#define SUBTYPE_PROBE_REQUEST 	0b0100
+#define SUBTYPE_PROBE_RESPONSE	0b0101
+#define SUBTYPE_BEACON 					0b1000
+
 #define	WIFI_CHANNEL_MAX		(13)
 #define	WIFI_CHANNEL_SWITCH_INTERVAL	(500)
 
-static wifi_country_t wifi_country = {.cc="CN", .schan=1, .nchan=13, .policy=WIFI_COUNTRY_POLICY_AUTO};
-
-typedef struct {
-	unsigned frame_ctrl:16;
-	unsigned duration_id:16;
-	uint8_t addr1[6]; /* receiver address */
-	uint8_t addr2[6]; /* sender address */
-	uint8_t addr3[6]; /* filtering address */
-	unsigned sequence_ctrl:16;
-	char other[12];
-	unsigned id:8;
-	unsigned length:8;
-	char addr4[32]; /* optional */
-} wifi_ieee80211_mac_hdr_t;
-
-typedef struct {
-	wifi_ieee80211_mac_hdr_t hdr;
-	uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
-} wifi_ieee80211_packet_t;
-
+/* Prototipi */
+static wifi_country_t wifi_country = { .cc = "CN", .schan = 1, .nchan = 13,
+		.policy = WIFI_COUNTRY_POLICY_AUTO };
 static esp_err_t event_handler(void *ctx, system_event_t *event);
 static void wifi_sniffer_init(void);
 static void wifi_sniffer_set_channel(uint8_t channel);
-static const char *wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type);
-static void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type);
+static const char *wifi_sniffer_packet_type2str(
+		wifi_promiscuous_pkt_type_t type);
+const char *
+wifi_sniffer_packet_subtype2str(uint8_t type);
+static void wifi_sniffer_packet_handler(void *buff,
+		wifi_promiscuous_pkt_type_t type);
+static void getMAC(char *addr, const uint8_t* data, uint16_t offset);
+static void printDataSpan(uint16_t start, uint16_t size, const uint8_t* data);
 
-
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK;
-}
-
-void
-wifi_sniffer_init(void)
-{
-		nvs_flash_init();
-	    tcpip_adapter_init();
-	    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-	    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-		ESP_ERROR_CHECK( esp_wifi_set_country(&wifi_country) ); /* set country for channel range [1, 13] */
-	    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-	    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_APSTA) );
-	    wifi_config_t sta_config = {
-	        .sta = {
-	            .ssid = "wifimio",
-	            .password = "qwertyui",
-	            .bssid_set = false
-	        }
-	    };
-	ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-	ESP_ERROR_CHECK( esp_wifi_start() );
-	ESP_ERROR_CHECK( esp_wifi_connect() );
-	esp_wifi_set_promiscuous(true);
-	esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
-}
-
-void app_main(void)
-{
+/* Main Loop */
+void app_main(void) {
 
 	uint8_t level = 0, channel = 1;
 
 	wifi_sniffer_init();
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    /* loop */
-    	while (true) {
-    		gpio_set_level(GPIO_NUM_4, level ^= 1);
-    		vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
-    		wifi_sniffer_set_channel(channel);
-    		channel = (channel % WIFI_CHANNEL_MAX) + 1;
-        	}
+	gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+	/* loop */
+	while (true) {
+		gpio_set_level(GPIO_NUM_4, level ^= 1);
+		vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
+		wifi_sniffer_set_channel(channel);
+		channel = (channel % WIFI_CHANNEL_MAX) + 1;
+	}
 }
 
-void
-wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
-{
+esp_err_t event_handler(void *ctx, system_event_t *event) {
+	return ESP_OK;
+}
 
-	if (type != WIFI_PKT_MGMT)
+void wifi_sniffer_init(void) {
+	nvs_flash_init();
+	tcpip_adapter_init();
+	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT()
+	;
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+	ESP_ERROR_CHECK(esp_wifi_set_country(&wifi_country)); /* set country for channel range [1, 13] */
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+	ESP_ERROR_CHECK(esp_wifi_start());
+	esp_wifi_set_promiscuous(true);
+	esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
+}
+
+static void getMAC(char *addr, const uint8_t* data, uint16_t offset) {
+	sprintf(addr, "%02x:%02x:%02x:%02x:%02x:%02x", data[offset + 0],
+			data[offset + 1], data[offset + 2], data[offset + 3],
+			data[offset + 4], data[offset + 5]);
+}
+
+static void printDataSpan(uint16_t start, uint16_t size, const uint8_t* data) {
+	for (uint16_t i = start; i < start + size; i++) {
+		printf("%c", data[i]);
+	}
+}
+void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type) {
+	const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *) buff;
+
+	uint8_t frameControl = (unsigned int) ppkt->payload[0];
+
+	uint8_t version = (frameControl & 0x03) >> 0;
+	uint8_t frameType = (frameControl & 0x0C) >> 2;
+	uint8_t frameSubType = (frameControl & 0xF0) >> 4;
+
+	// Only look for probe request packets
+	if (frameType != TYPE_MANAGEMENT || frameSubType != SUBTYPE_PROBE_REQUEST)
 		return;
 
-	const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
-	const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
-	const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
-	char ssid[hdr->length+1];
-	strncpy(ssid, (const char*)hdr->addr4, hdr->length);
-	ssid[hdr->length] = '\0';
+	printf("TYPE= %s", wifi_sniffer_packet_subtype2str(frameSubType));
+	printf(" RSSI: %02d", ppkt->rx_ctrl.rssi);
+	printf(" Ch: %02d", ppkt->rx_ctrl.channel);
 
-	printf("PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
-		" ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
-		" ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
-		" ADDR3=%02x:%02x:%02x:%02x:%02x:%02x,",
-		wifi_sniffer_packet_type2str(type),
-		ppkt->rx_ctrl.channel,
-		ppkt->rx_ctrl.rssi,
-		/* ADDR1 */
-		hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
-		hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
-		/* ADDR2 */
-		hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
-		hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
-		/* ADDR3 */
-		hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
-		hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
-	);
-	printf(" SSID = %s\n", ssid);
+	char addr[] = "00:00:00:00:00:00";
+	getMAC(addr, ppkt->payload, 10);
+	printf(" Peer MAC: %s", addr);
+
+	uint8_t SSID_length = ppkt->payload[25];
+	if (SSID_length > 0) {
+		printf(" SSID: ");
+		printDataSpan(26, SSID_length, ppkt->payload);
+	}
+	printf("\n");
+
 }
 
-void
-wifi_sniffer_set_channel(uint8_t channel)
-{
-
+void wifi_sniffer_set_channel(uint8_t channel) {
 	esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
 }
 
 const char *
-wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type)
-{
-	switch(type) {
-	case WIFI_PKT_MGMT: return "MGMT";
-	case WIFI_PKT_DATA: return "DATA";
+wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type) {
+	switch (type) {
+	case WIFI_PKT_MGMT:
+		return "MGMT";
+	case WIFI_PKT_DATA:
+		return "DATA";
 	default:
-	case WIFI_PKT_MISC: return "MISC";
+	case WIFI_PKT_MISC:
+		return "MISC";
+	}
+}
+
+
+const char *
+wifi_sniffer_packet_subtype2str(uint8_t type) {
+	switch (type) {
+	case SUBTYPE_PROBE_REQUEST:
+		return "PREQ";
+	case SUBTYPE_PROBE_RESPONSE:
+		return "PRES";
+	case SUBTYPE_BEACON:
+		return "BEAC";
+	default:
+		return "OTHE";
 	}
 }
 
