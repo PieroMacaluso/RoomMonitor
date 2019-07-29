aa036c0832f1b25431b1d543b7ba0a5d0482ce3c
vale95 <38795128+vale95@users.noreply.github.com>
2019-07-29 12:28:11 +0200

    Da_Testare
    Dovrebbe esserci tutto(pch non serve). Deve essere testato. L'ho fatto con Visual studio. Lorenzo se trovi poi dei problemi e non riesci a risolvere contattami che ci proviamo assieme.  Manca la query che farò poi io su QT.  Piero e Angelo avete già iniziato la parte ricezione multithread?(altrimenti posso iniaziare io a fare lo scheletro)

---
+++ Aggregazione/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+// ConsoleApplication1.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include "pch.h"
+#include "Aggregazione.h"
+#include <iostream>
+#include <thread>  
+
+int main()
+{//async ??
+	Aggregazione aggrega;
+    //std::cout << "Hello World!\n"; 
+	std::thread first(&aggrega);
+	first.join();
+}
+
+// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
+// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
+
+// Suggerimenti per iniziare: 
+//   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
+//   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
+//   3. Usare la finestra di output per visualizzare l'output di compilazione e altri messaggi
+//   4. Usare la finestra Elenco errori per visualizzare gli errori
+//   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
+//   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln
