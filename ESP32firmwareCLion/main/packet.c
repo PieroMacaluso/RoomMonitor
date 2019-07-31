2418b377a09f9f9b48bb4ed5af1fd7c6c8e7db91
angeloturco <softair023>
2019-07-31 05:43:48 -0700

    Risolta eccezione generata in caso di lista pacchetti vuota ma head not null

--- ESP32firmwareCLion/main/packet.c
+++ ESP32firmwareCLion/main/packet.c
@@ -177,7 +177,7 @@ void free_packet_list(node_t h) {
 void reset_packet_list(node_t h) {
     // If the HEAD is NULL or the head is not null but there aren't packets , simply returns
     if (h == NULL) return;
-    if (h->packet == NULL) return;  //todo da testare appena non ho dispositivi accesi che mi circondano
+    if (h->packet == NULL) return;
 
     if (h->next != NULL) {
         // Next is not NULL, so Recursive Free on nodes!
