303728ddb4945d7f43fddb1eddb43e3b6fb4c0e3
Angelo <s217167@studenti.polito.it>
2019-08-03 17:46:05 +0200

    Bozza unione vari elementi in un unico progetto con aggiunta di classe DequeThreadSafe.

---
+++ Server/cmake-build-debug/CMakeFiles/Server.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/polito/PDSproject/Server/Aggregazione.cpp" "D:/polito/PDSproject/Server/cmake-build-debug/CMakeFiles/Server.dir/Aggregazione.cpp.obj"
+  "D:/polito/PDSproject/Server/DequeThreadSafe.cpp" "D:/polito/PDSproject/Server/cmake-build-debug/CMakeFiles/Server.dir/DequeThreadSafe.cpp.obj"
+  "D:/polito/PDSproject/Server/Packet.cpp" "D:/polito/PDSproject/Server/cmake-build-debug/CMakeFiles/Server.dir/Packet.cpp.obj"
+  "D:/polito/PDSproject/Server/Ricezione.cpp" "D:/polito/PDSproject/Server/cmake-build-debug/CMakeFiles/Server.dir/Ricezione.cpp.obj"
+  "D:/polito/PDSproject/Server/cmake-build-debug/Server_autogen/mocs_compilation.cpp" "D:/polito/PDSproject/Server/cmake-build-debug/CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.obj"
+  "D:/polito/PDSproject/Server/main.cpp" "D:/polito/PDSproject/Server/cmake-build-debug/CMakeFiles/Server.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CHARTS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Server_autogen/include"
+  "D:/Qt/5.12.3/mingw73_64/include"
+  "D:/Qt/5.12.3/mingw73_64/include/QtCore"
+  "D:/Qt/5.12.3/mingw73_64/./mkspecs/win32-g++"
+  "D:/Qt/5.12.3/mingw73_64/include/QtGui"
+  "D:/Qt/5.12.3/mingw73_64/include/QtANGLE"
+  "D:/Qt/5.12.3/mingw73_64/include/QtWidgets"
+  "D:/Qt/5.12.3/mingw73_64/include/QtCharts"
+  "D:/Qt/5.12.3/mingw73_64/include/QtNetwork"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Server/cmake-build-debug/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "Server_autogen"
+  "CMakeFiles/Server_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/Server.dir/main.cpp.obj"
+  "CMakeFiles/Server.dir/Packet.cpp.obj"
+  "CMakeFiles/Server.dir/DequeThreadSafe.cpp.obj"
+  "CMakeFiles/Server.dir/Aggregazione.cpp.obj"
+  "CMakeFiles/Server.dir/Ricezione.cpp.obj"
+  "Server.pdb"
+  "Server.exe"
+  "Server.exe.manifest"
+  "libServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
