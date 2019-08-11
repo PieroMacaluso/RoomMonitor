bbea2c74dacaf34158301be6224eaa34ba32cbb2
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-08-11 17:35:47 +0200

    PositionData Modded

--- RoomMonitor/PositionData.cpp
+++ RoomMonitor/PositionData.cpp
@@ -4,11 +4,20 @@
 
 #include "PositionData.h"
 
-void PositionData::addPacket(float new_x, float new_y) {
+void PositionData::addPacket(double new_x, double new_y) {
     // Running Average (media mobile) utilizzata per mediare le posizioni.
     this->x = this->x + (new_x - this->x) / (this->n_packets + 1);
     this->y = this->y + (new_y - this->y) / (this->n_packets + 1);
     this->n_packets++;
+
+}
+
+void PositionData::addPacketWeighted(double new_x, double new_y, double rsum) {
+    // Running Average (media mobile) utilizzata per mediare le posizioni.
+    this->x = this->x + ((new_x / rsum) - this->x) / (this->n_packets + 1 / rsum);
+    this->y = this->y + ((new_y / rsum) - this->y) / (this->n_packets + 1 / rsum);
+    this->n_packets += 1 / rsum;
+
 
 }
 
@@ -17,6 +26,13 @@ void PositionData::addPacket(PositionData new_pos) {
     this->x = this->x + (new_pos.x - this->x) / (this->n_packets + 1);
     this->y = this->y + (new_pos.y - this->y) / (this->n_packets + 1);
     this->n_packets++;
+}
+
+void PositionData::addPacketWeighted(PositionData new_pos, double rsum) {
+    // Running Average (media mobile) utilizzata per mediare le posizioni.
+    this->x = this->x + (new_pos.x / rsum - this->x) / (this->n_packets + 1 / rsum);
+    this->y = this->y + (new_pos.y / rsum - this->y) / (this->n_packets + 1 / rsum);
+    this->n_packets += 1 / rsum;
 }
 
 std::ostream &operator<<(std::ostream &os, const PositionData &a) {
--- RoomMonitor/PositionData.h
+++ RoomMonitor/PositionData.h
@@ -10,14 +10,32 @@
 
 class PositionData {
     int n_packets;
-    float x;
-    float y;
+    double x;
+    double y;
 public:
-    PositionData(float x, float y): x(x), y(y), n_packets(1){};
-    void addPacket(float new_x, float new_y);
-    void addPacket(PositionData new_pos);
-    friend std::ostream& operator<<(std::ostream& os, const PositionData &a);
+    PositionData(double x, double y) : x(x), y(y), n_packets(1) {}
 
+    PositionData() : x(0), y(0), n_packets(0) {};
+
+    void addPacket(double new_x, double new_y);
+
+    void addPacketWeighted(double new_x, double new_y, double rsum);
+
+    void addPacket(PositionData new_pos);
+
+    void addPacketWeighted(PositionData new_pos, double rsum);
+
+
+    friend std::ostream &operator<<(std::ostream &os, const PositionData &a);
+
+
+    double getX() const {
+        return x;
+    }
+
+    double getY() const {
+        return y;
+    }
 };
 
 
