12a610e0d8d0fa761731c58a1377b291d51ed991
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-09-23 23:15:33 +0200

    Documentazione MonitoringPlotView

--- RoomMonitor/windows/plots/monitoring/MonitoringPlotView.h
+++ RoomMonitor/windows/plots/monitoring/MonitoringPlotView.h
@@ -11,19 +11,46 @@
 
 
 class MonitoringPlotView : public QChartView {
+    /**
+     * Grafico contenuto dalla View
+     */
     MonitoringChart * chart;
+
+    /**
+     * Callout utilizzato da MouseHover
+     */
     Callout *m_tooltip = nullptr;
+
 public:
+    /**
+     * Costruttori che ricevono il widget parente per la liberazione successiva
+     * @param parent
+     */
     explicit MonitoringPlotView(QWidget *parent = nullptr);
 
     explicit MonitoringPlotView(QChart *chart, QWidget *parent = nullptr);
 
+    /**
+     * Settaggio del puntatore al grafico da visualizzare
+     * @param chart grafico MonitoringChart
+     */
     void setChart(MonitoringChart *chart);
 
+    /**
+     * Gestione evento doppio click
+     * @param event
+     */
     void mouseDoubleClickEvent(QMouseEvent *event);
 
+    /**
+     * Gestione rotella del mouse
+     * @param event
+     */
     void wheelEvent(QWheelEvent *event);
-
+    /**
+     * Gestione callout
+     * @param event
+     */
     void tooltip(QPointF point, bool state);
 };
 
