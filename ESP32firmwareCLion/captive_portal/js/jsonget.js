5460979a631ca8e101230187399a7997f3bd61c1
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-09-26 01:21:44 +0200

    Captive Portal updated, ma tanti problemi su schedine.

--- ESP32firmwareCLion/captive_portal/js/jsonget.js
+++ ESP32firmwareCLion/captive_portal/js/jsonget.js
@@ -1,8 +1,10 @@
-$.getJSON("/data.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-    var str = JSON.stringify(json, null, 2);
-    str = str.replace(/,/g, "<br>");
-    str = str.replace("{", "");
-    str = str.replace("}", "");
-    document.getElementById("json").appendChild(document.createElement('pre')).innerHTML = str;
-});
+$.get("/data.json", function(data){
+    console.log(data);
+    document.getElementById("id").value = data.id ? data.id : "";
+    document.getElementById("ssid_ap").value = data.ssid_ap ? data.ssid_ap : "";
+    document.getElementById("password_ap").value = data.password_ap ? data.password_ap : "";
+    document.getElementById("channel").value = data.channel ? data.channel : "";
+    document.getElementById("ssid_server").value = data.ssid_server ? data.ssid_server : "";
+    document.getElementById("password_server").value = data.password_server ? data.password_server : "";
+    document.getElementById("ip_server").value = data.ip_server ? data.ip_server : "";
+}, "json");
