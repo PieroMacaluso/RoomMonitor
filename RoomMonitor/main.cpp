344c6f84b2830efa552965bf776aa10a87095d6a
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-11-12 11:24:23 +0100

    [APP] `main.cpp` refactoring

--- RoomMonitor/main.cpp
+++ RoomMonitor/main.cpp
@@ -1,36 +1,18 @@
-#include <iostream>
 #include <QApplication>
-#include <QDebug>
-#include <QMainWindow>
-#include <QtCore/QSettings>
-#include <QtWidgets/QStyleFactory>
 #include "monitoring/MonitoringServer.h"
 #include "windows/MainWindow.h"
 #include "windows/SettingDialog.h"
 
 /**
- * Questo è il main principale, da cui tutto parte
+ * Starting main
  */
 int main(int argc, char **argv) {
     QApplication a{argc, argv};
-    /*QSettings su{"VALP","RoomMonitoring"};
-    su.clear();*/
     int ret;
-    // TODO: eliminazione TRY CATCH?
-
-    try {
-        // Controllo che le impostazioni siano già state configurate, altrimenti riconfiguro
-        SettingDialog::settingCheckUp();
-        MainWindow w;
-        w.show();
-        ret = QApplication::exec();
-    } catch (const std::exception & e) {
-        // clean up here, e.g. save the session
-        // and close all config files.
-        std::cout << e.what() << std::endl;
-        return EXIT_FAILURE; // exit the application
-    }
-
+    // Check settings, otherwise set them to default values
+    SettingDialog::settingCheckUp();
+    MainWindow w;
+    w.show();
+    ret = QApplication::exec();
     return ret;
-
 }
